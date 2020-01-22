e2a1f590ea3690ff77848de998d0a2baad155189
Anderson <andersondejesus2011@gmail.com>
2020-01-22 10:04:36 -0430

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA2jTUN1-LY9GvAHKBWrf_SjETzf_W9x-w',
+    authDomain: 'ingreso-egreso-912f0.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-912f0.firebaseio.com',
+    projectId: 'ingreso-egreso-912f0',
+    storageBucket: 'ingreso-egreso-912f0.appspot.com',
+    messagingSenderId: '363021279013',
+    appId: '1:363021279013:web:58927a23e9b3a7381ec4cc',
+    measurementId: 'G-PC73KDJKRV'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA2jTUN1-LY9GvAHKBWrf_SjETzf_W9x-w',
+    authDomain: 'ingreso-egreso-912f0.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-912f0.firebaseio.com',
+    projectId: 'ingreso-egreso-912f0',
+    storageBucket: 'ingreso-egreso-912f0.appspot.com',
+    messagingSenderId: '363021279013',
+    appId: '1:363021279013:web:58927a23e9b3a7381ec4cc',
+    measurementId: 'G-PC73KDJKRV'
+  }
 };
 
 /*
